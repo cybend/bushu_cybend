958260d6e3ed6a8d49c7faa5a43bc7d8c0b33128
cybend <cybend@qq.com>
2015-03-24 16:34:38 +0800

    v1.0
    v1.0

---
+++ apt/12.04/apt12.04.sh
@@ -0,0 +1,17 @@
+# Version:cybendywauto-apt-1.0
+# By cybend
+# Title:自动化部署apt环境
+# ver:1.0
+# ubuntu 12.04
+
+echo  "\033[44;37m ================================ \033[0m"
+echo  "\033[44;37m 采贝技术服务器自动化部署平台 \033[0m"
+echo  "\033[44;37m ================================ \033[0m"
+echo  "\033[44;37m     【By 采贝技术运维团队】 \033[0m"
+
+cp /etc/apt/sources.list   /etc/apt/sources.list.tqkbak
+rm -rf /etc/apt/sources.list
+wget -P  /etc/apt/   http://cybend.com/bushu/php/12.04/sources.list
+apt-get update
+echo  "\033[44;37m     sources.list 更新完毕 \033[0m"
+sleep 10
---
+++ publish/publish_config.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+echo  "\033[44;37m ================================ \033[0m"
+echo  "\033[44;37m 采贝技术游戏服务器自动化部署平台 \033[0m"
+echo  "\033[44;37m ================================ \033[0m"
+echo  "\033[44;37m     【By 采贝技术运维团队】 \033[0m"
+
+Init_File="/etc/init.d/tqknodejs"
+Backup_Dir="/backup"
+Now=$(date +%Y%m%d)
+
+if [ -d "$Backup_Dir" ] ; then
+
+	echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+	echo "\033[41;37m backup文件夹已经存在，不需要创建! \033[0m"
+	sleep 10
+
+
+
+else
+
+	mkdir /backup
+	echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+	echo "\033[41;37m /backup文件夹创建成功! \033[0m"
+	sleep 10
+
+fi
+
+mkdir /backup/ver$Now
+echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+echo "\033[41;37m 备份目录建立成功! \033[0m"
+sleep 10
+
+tar zcvf /backup/ver$Now/soserver.bak.ver$Now.tar.gz  /soserver
+echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+echo "\033[41;37m 备份旧文件成功! \033[0m"
+sleep 10
+
+rm -rf /soserver
+tar zxvf /Release/soserver.ver$Now.tar.gz -C  /
+echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+echo "\033[41;37m 解压成功! \033[0m"
+sleep 10
+
+
+if [ -f "$Init_File" ] ; then
+
+	echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+	echo "\033[41;37m 自动启动脚本已存在! \033[0m"
+	sleep 10
+
+else
+
+	wget -P /etc/init.d/  http://cybend.com/bushu/initd/tqknodejs
+	chmod 777 /etc/init.d/tqknodejs
+	echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+	echo "\033[41;37m 自动启动脚本制作成功! \033[0m"
+	sleep 10
+
+fi
+ 
+/etc/init.d/tqknodejs start
+echo  "\033[44;37m 采贝技术运维团队提示： \033[0m"
+echo "\033[41;37m 启动成功! \033[0m"
+sleep 10
+
+
+ps -ef |grep node
+
